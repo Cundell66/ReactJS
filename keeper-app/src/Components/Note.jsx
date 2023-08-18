a808ffc5eb51204114f6eb94a55646966eeb62ca
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-18 13:43:00 +0100

    keeper 2 solved

--- keeper-app/src/Components/Note.jsx
+++ keeper-app/src/Components/Note.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Note() {
-return (<div className='note'>
-        <h1> This is the note title </h1>
-        <p> This is the note content </p>
-    </div>);
-}
-//5. Create a Note.jsx component to show a <div> element with a
+function Note(props) {
+    return (
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+      </div>
+    );
+  }
+  //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 
 
