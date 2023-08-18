77ea98201ab1ccadf863e7eb79f18df727c0f081
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-18 13:53:17 +0100

    keeper 2 completed

---
+++ keeper-app/src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+import notes from "../notes";
+
+function App() {
+    return (<div>
+        <Header />,
+        {notes.map(Note)}
+        <Footer />
+    </div>);
+}
+
+export default App;
